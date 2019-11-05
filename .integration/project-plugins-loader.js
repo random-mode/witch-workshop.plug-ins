8066b9e31cac2810bb7f050b3af9d5483d2389a0
Arnaud LEPESANT (acs-l) <arnaud.lepesant@gmail.com>
2019-11-05 01:46:40 +0100

    [#2] Starting implementation of the script responsible to load plugins

---
+++ .integration/project-plugins-loader.js
@@ -0,0 +1,42 @@
+﻿'use strict';
+
+/*:
+ * @plugindesc
+ * Loads project's specific plugins.
+ *
+ * @author acs-l
+ *
+ * @param Plugins' directory
+ * @desc Path to the directory where plugins'-being-developed are stored
+ * Default value: witch-workshop.plug-ins/plugins
+ * @default witch-workshop.plug-ins/plugins
+ */
+
+const Project = {};
+
+/**
+ * Returns the filename of the current script (i.e.: the one which is invoking this function).
+ * @return {String} the current script's filename.
+ */
+Project.filename = () => {
+  const scripts = document.getElementsByTagName('script');
+  const path = scripts[scripts.length - 1].src;
+  const filename = path.substring(
+    path.lastIndexOf('/') + 1,
+    path.lastIndexOf('.')
+  );
+  return filename;
+};
+
+/**
+ * Short-hand to constructs a new plugin representation (as used in `plugins.list.js`).
+ * @param {String} `name` the plugin's short name
+ * @param {String} `source` the plugin's main script's location
+ * @eturn the structure which holds one plugin's information.
+ */
+Project.Plugin = (name, source) => {
+  return {
+    name: name,
+    source: source,
+  };
+};
