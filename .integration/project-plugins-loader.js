fbb86fe6502e9668d5450db4edda9cceeefde2f8
Arnaud LEPESANT (acs-l) <arnaud.lepesant@gmail.com>
2019-11-05 01:51:28 +0100

    [#2] Implementing project's plugins' loading mechanism

--- .integration/project-plugins-loader.js
+++ .integration/project-plugins-loader.js
@@ -89,3 +89,71 @@ Project.Plugin = (name, source) => {
     source: source,
   };
 };
+
+// Plug-in's setup
+(function(context) {
+  // Parameters' formatter
+  const string = default_value => current_value => {
+    return (current_value || '').trim() || default_value;
+  };
+
+  // Parameters' retrieval
+  const parameters = PluginManager.parameters(Project.filename());
+  const plugins_dir = string('witch-workshop.plug-ins/plugins')(
+    parameters["Plugins' directory"]
+  );
+  const plugins_list_src = `${plugins_dir}/plugins.list.js`;
+
+  const load_main_script = plugin => callback_once_loaded => {
+    const src = `${plugins_dir}/${plugin.source}`;
+    console.log(
+      ...Project.log.format(
+        `Loading main script of project's plugin: ${plugin.name} (${src})`
+      )
+    );
+    Project.load_script(src)(callback_once_loaded);
+  };
+
+  const load_plugin = plugin => callback_once_loaded => {
+    return () => {
+      Project.log.group(`Loading project's plugin: ${plugin.name}`)(
+        end_plugin_group => {
+          load_main_script(plugin)(() => {
+            end_plugin_group();
+            callback_once_loaded();
+          });
+        }
+      );
+    };
+  };
+
+  const load_plugins_synchronously = callback_once_finished => {
+    return () => {
+      if (Array.isArray(Project.plugins)) {
+        Project.plugins.reverse().reduce(
+          (Σ, p) => load_plugin(p)(Σ),
+          () => {
+            console.log(
+              ...Project.log.format("Project's plugins load is complete")
+            );
+            callback_once_finished();
+          }
+        )();
+      } else {
+        console.log(
+          ...Project.log.format(
+            `${plugins_list_src}#Project.Plugin is wrongly formatted!`
+          )
+        );
+        callback_once_finished();
+      }
+    };
+  };
+
+  Project.log.group_rmex(
+    `Loading file containing the list of project's plugins: ${plugins_dir}/plugins.list.js`
+  )(end_log_group => {
+    const load_plugins = load_plugins_synchronously(end_log_group);
+    Project.load_script(plugins_list_src)(load_plugins);
+  });
+})(this);
